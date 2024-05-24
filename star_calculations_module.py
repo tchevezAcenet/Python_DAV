c93ba77d4769c0c35249547de3dd8e3e7f8b84f5
tchevezAcenet <163461125+tchevezAcenet@users.noreply.github.com>
2024-05-24 17:17:50 -0230

    Add files via upload

---
+++ star_calculations_module.py
@@ -0,0 +1,19 @@
+#star_calculations_module.py
+
+import numpy as np
+
+def distance_from_earth_parsecs(Mv,m):
+    """
+    Calculate the distance in parsecs between each star and Earth based on their Absolute Magnitude and apparent magnitude.
+    """
+    distance_parsecs = #Your code goes here
+    return distance_parsecs
+
+def parsecs_to_lightYears(distance_parsec):
+    """
+    Convert the distance in parsecs to light years units
+    """
+    distance_lightYears = #Your code goes here
+    
+    return distance_lightYears
+    
